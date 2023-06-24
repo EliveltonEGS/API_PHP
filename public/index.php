67ae9c6bd35278e5984b7438508ce59f8333ea96
Elivelton <eliveltongsoficial@gmail.com>
2023-06-24 16:27:29 -0300

    Fix Cors API

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../db/database.php';
 require_once __DIR__ . '/../routes/routes.php';
